7900bc775a4555b17965a11d934bfa9b221c3ff9
hexing wang <358098351@qq.com>
2025-07-19 00:59:46 +0800

    house_model

--- src/components/heroModels/HeroExperience.jsx
+++ src/components/heroModels/HeroExperience.jsx
@@ -4,13 +4,14 @@
  * @Author: Hersynne
  * @Date: 2025-07-14 17:42:50
  * @LastEditors: Hersynne
- * @LastEditTime: 2025-07-17 19:09:57
+ * @LastEditTime: 2025-07-19 00:59:09
  */
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useMediaQuery } from 'react-responsive'
 import { HeroModel } from './HeroModel'
+import { TestModel } from './TestModel'
 
 
 const HeroExperience = () => {
@@ -18,18 +19,28 @@ const HeroExperience = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
 
     return (
-        <Canvas camera={{ position: [0, 2, 15], fov: 45 }}>
+        <Canvas camera={{ position: [0, 0, 12], fov: 45 }}>
             <OrbitControls
                 enableZoom={!isTablet}
-             // enablePan={false}
+            // enablePan={false}
             // maxDistance={20}
             // minDistance={5}
             // minPolarAngle={Math.PI / 5}
             // maxPolarAngle={Math.PI / 2}
             />
             <ambientLight intensity={0.2} color='1a1a40' />
-            <directionalLight position={[5, 5, 5]} intensity={5} />
-            <HeroModel />
+            <directionalLight position={[5, 5, 5]} intensity={1} />
+            <Suspense fallback={null}>
+                <group
+                    scale={isMobile ? 0.7 : 1}
+                    position={[0, -1.5, 0]}
+                    rotation={[0, -Math.PI / 4, 0]}
+                >
+                    <HeroModel />
+                    {/* <TestModel/> */}
+                </group>
+            </Suspense>
+
         </Canvas>
     )
 }
